6dd1ce832c02ecbccc6c80d370257b4a44300880
gcassella <g.cassella20@imperial.ac.uk>
2020-11-12 11:07:49 +0000

    Tidying up

--- he_dmc.py
+++ he_dmc.py
@@ -44,57 +44,6 @@ def Wavefunction(f, parameters):
 
     return wf
 
-#class Wavefunction():
-#    def __init__(self, f, params):
-#        self.iscomplex = False
-#        self._value = 0
-#
-#        self.f = f
-#        self.parameters = {"p" : params}
-#
-#        self.log_pgrad = jax.grad(lambda p, c: jnp.log(jnp.abs(self.f(p, c))))
-#        self.log_grad = lambda p, c, e: jax.grad(lambda p, c: jnp.log(jnp.abs(self.f(p, c))), 1)(p, c)[e]
-#        self.hess = jax.jacfwd(jax.grad(self.f, 1), 1)
-#        self.second_derivs = lambda p, c: jnp.diag(self.hess(p, c).reshape(c.shape[0]*c.shape[1], c.shape[0]*c.shape[1]))
-#        self.batch_log_grad = jax.jit(jax.vmap(self.log_grad, in_axes=(None, 0, None)))
-#        self.batch_log_pgrads = jax.jit(jax.vmap(self.log_pgrad, in_axes=(None, 0)))
-#        self.batch_second_derivs = jax.jit(jax.vmap(self.second_derivs, in_axes=(None, 0)))
-#        self.batch_predict = jax.jit(jax.vmap(self.f, in_axes=(None, 0)))
-#
-#    def recompute(self, configs):
-#        self.configs = configs
-#        self._value = self.batch_predict(self.parameters["p"], self.configs)
-#        return jnp.array(jnp.log(jnp.abs(self._value)))
-#
-#    def updateinternals(self, e, epos, mask=None):
-#        self.configs = jax.ops.index_update(self.configs, jax.ops.index[:, e, :], epos)
-#        self._value = self.batch_predict(self.parameters["p"], self.configs)
-#        return jnp.array(jnp.log(jnp.abs(self._value)))
-#
-#    def value(self):
-#        return jnp.array(jnp.log(jnp.abs(self._value)))
-#
-#    def testvalue(self, e, epos, mask=None):
-#        new_configs = jax.ops.index_update(self.configs, jax.ops.index[:, e, :], epos)
-#
-#        return jnp.array(self.batch_predict(self.parameters["p"], new_configs) / self._value)
-#    
-#    def pgradient(self):
-#        log_pgrads = self.batch_log_pgrads(self.parameters["p"], self.configs)
-#        log_grad_flat, _ = ravel_pytree(log_pgrads)
-#        
-#        return {"p": jnp.array(log_grad_flat)}
-#
-#    def gradient(self, e, epos):
-#        new_configs = jax.ops.index_update(self.configs, jax.ops.index[:, e, :], epos)
-#        return jnp.array(self.batch_log_grad(self.parameters["p"], new_configs, e).T)
-#
-#    def laplacian(self, e, epos):
-#        new_configs = jax.ops.index_update(self.configs, jax.ops.index[:, e, :], epos)
-#        second_derivs = self.batch_second_derivs(self.parameters["p"], new_configs)
-#        lapl = jnp.sum(second_derivs[:, e*3:(e+1)*3], axis=-1)
-#        return jnp.array(lapl / self.batch_predict(self.parameters["p"], new_configs))
-
 if __name__ == "__main__":
     import pyscf
     import pyqmc
@@ -112,7 +61,7 @@ if __name__ == "__main__":
     #simple_p = [1.6875]
     
     wf = Wavefunction(nn_hylleraas, nn_hylleraas_p)
-    nconfig = 1024
+    nconfig = 1000
     
     mol = pyscf.gto.M(atom="He 0. 0. 0.")
     
